a11d361edd0cc501c3f39560bf2adc620ddf4a4e
ogbonnaya_emmanuel <ogbonnayaemmanuel2030@gmail.com>
2022-06-30 16:07:14 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-/
-* main - Entry point
-* return: always 0 (Success)
-* /
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+
+int main(void)
+{ puts("Programming is like building a multilingual puzzle\n"); return (0); }
