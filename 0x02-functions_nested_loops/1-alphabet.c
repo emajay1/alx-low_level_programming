154df7b2dee4adb2828564d6c0ef86a68b024097
ogbonnaya_emmanuel <ogbonnayaemmanuel2030@gmail.com>
2022-07-06 03:19:35 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowrcase followed by a new line
+ */
+
+void print_alphabet(void);
+char c;
+
+{
+
+	c = a;
+	
+	for (c = 'a'; c <= 'z'; c++)
+
+	_putchar(c);
+	
+	_putchar('\n');
+
+}
+
+
