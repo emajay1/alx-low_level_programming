25cafbd594f36feaad33259a7c43b9b238a23db7
ogbonnaya_emmanuel <ogbonnayaemmanuel2030@gmail.com>
2022-07-30 01:11:24 -0700

    hard things

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,7 +9,6 @@
  *
  * Return: void
  */
-
 void print_line(char *c, int s, int l)
 {
 	int j, k;
@@ -23,7 +22,7 @@ void print_line(char *c, int s, int l)
 		if (j % 2)
 			putchar(' ');
 	}
-	for (k = 0; k <=5; k++)
+	for (k = 0; k <= s; k++)
 	{
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
