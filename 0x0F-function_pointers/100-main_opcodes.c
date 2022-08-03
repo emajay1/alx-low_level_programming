b95a0af8af3316502f92dadf6ad2c0fb717dbf84
ogbonnaya_emmanuel <ogbonnayaemmanuel2030@gmail.com>
2022-08-03 13:39:18 -0700

    task100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,7 +26,7 @@ printf("\n");
 /**
  * main - print the opcodes og its own main function
  * @argc: number of arguments passed to the function
- * @rgv: array of pointers to arguments
+ * @argv: array of pointers to arguments
  *
  * Return: 0 if successful
  */
