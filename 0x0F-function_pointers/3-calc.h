886e215c480dd2398a959645b5fb3781b29c61ed
ogbonnaya_emmanuel <ogbonnayaemmanuel2030@gmail.com>
2022-08-03 13:12:39 -0700

    task4

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _CALC_H
+#define _CALC_H
 #include <string.h>
 
 /**
@@ -10,9 +10,10 @@
  */
 typedef struct op
 {
-char *op;
-int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - select the correct operation function asked by user
@@ -18,7 +19,11 @@ op_t ops[] = {
 };
 int i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+while (ops[i].op != NULL)
+{
+	if (strcmp(s, ops[i].op) == 0)
+		break;
+	i++;
+}
 return (ops[i].f);
 }
